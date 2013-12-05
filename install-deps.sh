016ab67e6819e3432e04596a032ea103ac33928e
Ninerian <hallo@daniel-der-grosse.de>
2013-12-05 11:07:06 +0100

    Replaced addyosmani with myself, changed install-deps.sh to prevent errors

--- install-deps.sh
+++ install-deps.sh
@@ -3,21 +3,21 @@
 
 # https://github.com/jamiew/git-friendly
 # the `push` command which copies the github compare URL to my clipboard is heaven
-sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+#sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
 # https://rvm.io
 # rvm for the rubiess
-curl -L https://get.rvm.io | bash -s stable --ruby
+#curl -L https://get.rvm.io | bash -s stable --ruby
 
 # https://github.com/isaacs/nave
 # needs npm, obviously.
 # TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
-npm install -g nave
+#npm install -g nave
 
 
 # homebrew!
 # you need the code CLI tools YOU FOOL.
-ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+#ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
 
 
